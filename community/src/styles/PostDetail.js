3b1b32810efc5795083a59baf41f708646d9df15
JeongBeanHyun <kelly92108@gmail.com>
2025-09-19 01:21:32 +0900

    feat: 실습 코드 추가

---
+++ community/src/styles/PostDetail.js
@@ -0,0 +1,15 @@
+// PostDetail.js
+import styled from "styled-components";
+
+export const Title = styled.h2`
+  margin: 0 0 12px;
+`;
+
+export const Divider = styled.hr`
+  width: 100%;
+`;
+
+export const Article = styled.p`
+  white-space: pre-wrap;
+  line-height: 1.6;
+`;
